8813ff3a82e6a9392fd485d70cd7723f61db9d9a
Cybernetics <79071277+shade234sherif@users.noreply.github.com>
2022-08-14 23:22:04 +0100

    Add files via upload

--- MAIL.py
+++ MAIL.py
@@ -1,20 +1,20 @@
 import sys
-import requests
+from http.client import responses
 from ast import Break, Try, excepthandler
 import mechanize
 
 
 if sys.version_info[0] !=3:
-    print("please follow the usage commands!!")
+    print("please run tool with python3 : python3 MAIL.py!!")
     sys.exit()
 
 print('----------------------------------------------------------------')
 print('\033[1;31m ___  ___  ___   _____  _      _____ ______ \033[1;31m')
-print("|  \/  | / _ \ |_   _|| |    |  ___|| ___ \")
-print('\033[1;31m | .  . |/ /_\ \  | |  | |    | |__  | |_/ /\033[1;31m')
-print('\033[1;31m | |\/| ||  _  |  | |  | |    |  __| |    / \033[1;31m')
-print('\033[1;31m | |  | || | | | _| |_ | |____| |___ | |\ \ \033[1;32m')
-print('\033[1;31m \_|  |_/\_| |_/ \___/ \_____/\____/ \_| \_|\033[1;0m')
+print(' |  \/  | / _ \ |_   _|| |    |  ___|| ___ \\')
+print('\033[1;31m | .  . |/ /_\ \  | |  | |    | |__  | |_/ /\033[1;31')
+print('\033[1;31m | |\/| ||  _  |  | |  | |    |  __| |    / \033[1;31')
+print('\033[1;31m | |  | || | | | _| |_ | |____| |___ | |\ \ \033[1;32')
+print('\033[1;31m \_|  |_/\_| |_/ \___/ \_____/\____/ \_| \_|\033[1;0')
 print('----------------------------------------------------------------')
 print("\033[1;33mEXPLORE MORE IN HACKING - https://facebook.com/cyberhacks6\033[1;0m")
 print('\033[1;33mGITHUB PROFILE - https://github.com/shade234sherif\033[1;32m')
@@ -24,10 +24,10 @@ print("\033[1;32mANONY MAILER BY CYBERNETICS ~ MADE BY AN HACKER FOR HACKERS\033
 
 br = mechanize.Browser()
 
-to = input("Enter receivers mail address:")
-subject = input("Enter your message subject:")
-print ("Message:")
-message = input("Enter your message content:")
+to = input("Enter receivers mail address: ")
+subject = input("Enter your message subject: ")
+print ("Message: ")
+message = input("Enter your message content: ")
 
 url = "https://anonymouse.org/anonemail.html"
 headers = "Mozilla/4.0 (compatible; MSIE 5.0; AOL 4.0; Windows 95; c_athome)"
@@ -47,9 +47,9 @@ br.form['To'] = to
 br.form['Subject'] = subject
 br.form['Message'] = message
 
-response  = br.submit()
+responses  = br.submit()
 
-response_data = responses.r()
+responses_data = responses.r()
 
 if " The e-mail has been sent anonymously" in responses:
     print("\n The message has been successfully sent \n The recipient will get the message within 6hours!!! ")
@@ -65,4 +65,4 @@ ConnectionRefusedError
 Try
 print('\n please check your wifi ~ cellular data connection\n then RESTART SOFTWARE!!')
 
-sys.exit()
+sys.exit
